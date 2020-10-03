6396fda131a15035b58552c3c32f383e03b24635
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-03 09:54:18 +0300

    Add square example

---
+++ examples/square/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * @fn main
+ * @brief Entry point
+ * @return 0
+ */
+int main() {
+  int size = 10;    // Widh and height of square
+  char mark = '*';  // Square draw mark
+  for (int height = 0; height < size; height++) {
+    for (int width = 0; width < size; width++) {
+      printf("%c", mark);
+    }
+    printf("%s", "\n");
+  }
+  return 0;  // Exit with 0
+}
