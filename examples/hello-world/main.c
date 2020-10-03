dcf23449359f048e40c2cf0b71bcc5a66c298b63
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-03 09:46:08 +0300

    Update hello world

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include <stdio.h>  // Include for printf
 
+/**
+ * @fn main
+ * @brief Entry point
+ * @return 0
+ */
 int main() {
-  printf("Hello World");
-  return 0;
+  printf("Hello World");  // Print Hello World
+  return 0;               // Exit with 0
 }
