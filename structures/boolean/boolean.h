d5877fa97ec2eb137be434619a4dd66777d22fa4
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-10-03 10:12:57 +0300

    Add boolean structure

---
+++ structures/boolean/boolean.h
@@ -0,0 +1,22 @@
+#ifndef BOOLEAN_H
+#define BOOLEAN_H
+
+/**
+ * @typedef BOOLEAN
+ * @brief Typedef declare boolean
+ */
+typedef unsigned short BOOLEAN;
+
+/**
+ * @def true
+ * @brief True
+ */
+#define true 1
+
+/**
+ * @def false
+ * @brief False
+ */
+#define false 0
+
+#endif  // BOOLEAN_H
